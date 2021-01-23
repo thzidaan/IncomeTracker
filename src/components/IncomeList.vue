5f4ab1e37e393a414acdb158997f41a9aa6cbb4b
Tahmidul Hasnain Zidaan <thzidaan28@yahoo.com>
2021-01-22 21:54:39 -0600

    IncomeItem component added in IncomeList

--- src/components/IncomeList.vue
+++ src/components/IncomeList.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="income-list">
-
+    <IncomeItem v-for="data in state.income" :key="data.id" :income="data"/> 
   </div>
 </template>
 
 <script>
+import IncomeItem from './IncomeItem';
+
+
 export default {
+    props : {
+        state: Object
+    }
 
 }
 </script>
