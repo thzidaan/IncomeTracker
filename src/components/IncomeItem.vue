b5978567780c18ac4b66149f87d972348a7e4263
Tahmidul Hasnain Zidaan <thzidaan28@yahoo.com>
2021-01-22 21:49:30 -0600

    IncomeItem component created

---
+++ src/components/IncomeItem.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="income-item">
+      <div class="removeItem">x</div>
+      <div class="desc"> Description</div>
+      <div class="price">$0</div>
+      <div class="date">Date</div>
+  </div>
+</template>
+
+<script>
+export default {
+
+    props : {
+        income: Object
+    }
+}
+</script>
+
+<style>
+ .income-item {
+    position: relative;
+    display: flex;
+    padding: 15px 15px 15px 0px;
+    background-color: #FFF;
+    border-radius: 8px;
+    max-width: 600px;
+    margin: 0 auto 30px;
+  }
+  .removeItem {
+    color: #EF2D2D;
+    font-weight: 600;
+    font-size: 20px;
+    line-height: 1;
+    text-align: center;
+    margin: 0 15px;
+  }
+  .desc {
+    color: #666;
+    flex: 1 1 100%;
+    font-size: 20px;
+  }
+  .price {
+    color: #666;
+    min-width: 100px;
+    font-size: 20px;
+  }
+  .date {
+    color: #666;
+    text-align: right;
+    font-size: 20px;
+  }
+
+</style>
---
+++ src/components/IncomeList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="income-list">
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+.income-list{
+    margin-top: 30px;
+    padding: 15px;
+}
+
+</style>
